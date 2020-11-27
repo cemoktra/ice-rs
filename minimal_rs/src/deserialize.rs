30eac6870a923bb19b030400dfcf3c9b4e31f821
cemoktra <b.schubert82@gmail.com>
2020-11-27 20:32:12 +0100

    Remove moved files

--- minimal_rs/src/deserialize.rs
+++
@@ -1,5 +0,0 @@
-use crate::errors::Error;
-
-pub trait Deserialize {
-    fn from_bytes(bytes: &[u8]) -> Result<Self, Error>  where Self: Sized;
-}
--- minimal_rs/src/serialize.rs
+++
@@ -1,5 +0,0 @@
-use crate::errors::Error;
-
-pub trait Serialize {
-    fn to_bytes(&self) -> Result<Vec<u8>, Error>;
-}
