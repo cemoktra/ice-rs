72cf53381062964f293f1c06d43e8644a57585e0
cemoktra <b.schubert82@gmail.com>
2020-12-21 10:15:39 +0100

    finalize ice include

--- Ice/src/slice/module.rs
+++ Ice/src/slice/module.rs
@@ -38,7 +38,7 @@ impl UseStatements {
 pub struct Module {
     pub name: String,
     pub full_name: String,
-    sub_modules: Vec<Module>,
+    pub sub_modules: Vec<Module>,
     enumerations: Vec<Enum>,
     exceptions: Vec<Exception>,
     structs: Vec<Struct>,
@@ -72,16 +72,6 @@ impl Module {
 
     pub fn snake_name(&self) -> String {
         snakecase::to_snake_case(&self.name)
-    }
-
-    pub fn add_module(&mut self, module: Module) {
-        for sub_module in &self.sub_modules {
-            if sub_module.full_name == module.full_name {
-                todo!();
-                return;
-            }
-        }
-        self.sub_modules.push(module);
     }
 
     pub fn add_sub_module(&mut self, name: &str) -> Result<&mut Module, Box<dyn std::error::Error>> {
--- slice2rs/src/main.rs
+++ slice2rs/src/main.rs
@@ -8,6 +8,8 @@ use std::path::Path;
 #[derive(Clap)]
 #[clap(version = "1.0")]
 struct Opts {
+    #[clap(short)]
+    include_dir: Option<String>,
     slice_file: String,
     out_dir: String
 }
@@ -15,7 +17,14 @@ struct Opts {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts: Opts = Opts::parse();
     let slice_path = Path::new(&opts.slice_file);
-    let root = parser::parse_ice_file(Path::new(&slice_path))?;
+    let include_dir = match opts.include_dir.as_ref() {
+        Some(dir) => Path::new(dir),
+        None => {
+            // TODO: extract fome file(s)
+            Path::new(".")
+        }
+    };
+    let root = parser::parse_ice_file(&slice_path, include_dir)?;
     root.generate(
         Path::new(&opts.out_dir),
         &slice_path
