5488dd57119d412dd19b085b022ad50fd6728c06
cemoktra <b.schubert82@gmail.com>
2021-01-25 20:26:20 +0100

    Add context demo

---
+++ demos/ice/context/src/client/bin.rs
@@ -0,0 +1,10 @@
+mod gen;
+use crate::gen::demo::{Context, ContextPrx};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let comm = ice_rs::communicator::initialize("config.client");
+    let proxy = comm.property_to_proxy("Context.Proxy")?;
+    let mut context_prx = ContextPrx::unchecked_cast(proxy)?;
+
+    context_prx.call()
+}
