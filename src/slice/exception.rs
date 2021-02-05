3345b5893ddc5b9f838a242127deb24d6fa262f9
Bastian <b.schubert82@gmail.com>
2021-02-05 07:49:20 +0100

    fix generated exception code (#40)

--- src/slice/exception.rs
+++ src/slice/exception.rs
@@ -68,7 +68,7 @@ impl Exception {
             impl std::error::Error for #id_token {}
 
             impl ToBytes for #id_token {
-                fn to_bytes(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+                fn to_bytes(&self) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> {
                     let mut bytes = Vec::new();
                     #(#member_to_bytes_tokens);*;
                     Ok(bytes)
@@ -76,7 +76,7 @@ impl Exception {
             }
 
             impl FromBytes for #id_token {
-                fn from_bytes(bytes: &[u8], read_bytes: &mut i32) -> Result<Self, Box<dyn std::error::Error>>
+                fn from_bytes(bytes: &[u8], read_bytes: &mut i32) -> Result<Self, Box<dyn std::error::Error + Send + Sync>>
                 where Self: Sized {
                     let mut read = 0;
                     let _flag = SliceFlags::from_bytes(&bytes[read as usize..bytes.len()], &mut read)?;
